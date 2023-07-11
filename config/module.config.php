f97fadc0c6b209606004554864fc178112259758
Oliver Stöhr <stoehr@effective-webwork.de>
2023-07-11 17:14:49 +0200

    Remove custom PluginManager

--- config/module.config.php
+++ config/module.config.php
@@ -5,16 +5,18 @@ $config = [
     'service_manager' => [
         'allow_override' => true,
         'factories' => [
-            'Libraries\AjaxHandler\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Libraries\AjaxHandler\GetLibraries' => 'Libraries\AjaxHandler\GetLibrariesFactory',
             'Libraries\Search\BackendManager' => 'Libraries\Search\BackendManagerFactory',
-            'Libraries\Search\Params\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-            'Libraries\Search\Results\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Libraries\Search\Search2\Params' => 'Libraries\Search\Solr\ParamsFactory',
+            'Libraries\Search\Solr\Params' => 'Libraries\Search\Solr\ParamsFactory',
+            'VuFind\Search\Search2\Results' => 'Libraries\Search\Search2\ResultsFactory',
+            'VuFind\Search\Solr\Results' => 'Libraries\Search\Solr\ResultsFactory',
         ],
         'aliases' => [
-            'VuFind\AjaxHandler\PluginManager' => 'Libraries\AjaxHandler\PluginManager',
+            'getLibraries' => 'Libraries\AjaxHandler\GetLibraries',
+            'search2' => 'Libraries\Search\Search2\Params',
+            'solr' => 'Libraries\Search\Solr\Params',
             'VuFind\Search\BackendManager' => 'Libraries\Search\BackendManager',
-            'VuFind\Search\Params\PluginManager' => 'Libraries\Search\Params\PluginManager',
-            'VuFind\Search\Results\PluginManager' => 'Libraries\Search\Results\PluginManager',
         ],
     ],
 ];
